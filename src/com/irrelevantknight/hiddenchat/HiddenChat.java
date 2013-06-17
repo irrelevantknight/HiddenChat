3d7127807c2e4e762160e53efe2f1de5c83af0f7
irrelevantknight <irrelevantknight@gmail.com>
2013-06-17 10:37:18 -0400

    Fixed indentation

--- src/com/irrelevantknight/hiddenchat/HiddenChat.java
+++ src/com/irrelevantknight/hiddenchat/HiddenChat.java
@@ -4,7 +4,7 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class HiddenChat extends JavaPlugin {
-	
+
 	@Override
     public void onEnable() {
 		getServer().getPluginManager().registerEvents(new ServerChatPlayerListener(this), this);
